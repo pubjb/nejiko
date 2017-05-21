feed795ea1be3c6be74944c61ea3a6d8c6124d43
pubjb <pubjb@gmail.com>
2017-05-22 07:39:00 +0900

    first commit

---
+++ Assets/Scripts/SimpleFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour {
+    Vector3 diff;
+    public GameObject target;
+    public float followSpeed;
+
+	// Use this for initialization
+	void Start () {
+        diff = target.transform.position - transform.position;
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position,
+            target.transform.position - diff,
+            Time.deltaTime * followSpeed
+            );
+    }
+}
